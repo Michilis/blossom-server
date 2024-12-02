d5cf462568e49b18189bdb97139151663876fe5c
Michilis <120072772+Michilis@users.noreply.github.com>
2024-12-02 19:18:32 +0100

    Update whitelist.ts

--- src/helpers/whitelist.ts
+++ src/helpers/whitelist.ts
@@ -1,6 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import fetch from 'cross-fetch';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const WHITELIST_URL = 'https://relayapi.azzamo.net/.well-known/nostr.json';
 const WHITELIST_FILE = path.resolve(__dirname, '../data/whitelist.json');
